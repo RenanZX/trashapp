9276ac34c1859d88e31ea06d68a6571b5991eb24
RenanZX <renangodoimedeiros@gmail.com>
2021-10-25 12:10:32 -0300

    Adicionando Mapa

---
+++ forms/FormMapa.tsx
@@ -0,0 +1,82 @@
+import React, { Component } from "react";
+import { Button, TextInput, StyleSheet, View, Text } from 'react-native'
+import {primary_button_color, styles, colors} from '../styles'
+import {Props} from '../App'
+
+type State = {
+  title: string;
+  text: string;
+}
+
+
+const style_form = StyleSheet.create({
+  input_text: {
+    backgroundColor: colors.white, 
+    width: '100%', 
+    marginBottom: 10, 
+    borderColor: colors.gray, 
+    borderWidth: 2, 
+    borderRadius: 3
+  }
+})
+
+interface FormProps extends Props {
+  onConfirm: (text: string, title: string) => void;
+  onCancelar: () => void;
+}
+
+class FormMapa extends Component<FormProps, State> {
+
+  constructor(props: FormProps){
+    super(props)
+  }
+
+  state = {
+    title: "",
+    text: "",
+  }
+
+  render() {
+    const { onConfirm, onCancelar } = this.props;
+      return(
+      <View>
+        <Text style={{ fontWeight: 'bold' }}>Titulo:</Text>
+        <TextInput
+          style={style_form.input_text}
+          value={this.state.title}
+          onChangeText={text =>this.setState({title: text})}
+        />
+        <Text style={{ fontWeight: 'bold' }}>Descricao:</Text>
+        <TextInput
+          multiline={true}
+          numberOfLines={5}
+          style={style_form.input_text}
+          value={this.state.text}
+          onChangeText={text => this.setState({text: text})}
+        />
+        <View style={{ flexDirection: 'row' }}>
+          <View style={{flex:1}}>
+            <Button
+              color={primary_button_color}
+              onPress={() => {
+                  onConfirm.call(this, this.state.text, this.state.title);
+              }}
+              title="Salvar"
+            />
+          </View>
+
+          <View style={{flex:1, marginLeft: 10}}>
+            <Button
+              color={colors.red}
+              onPress={onCancelar}
+              title="Cancelar"
+            />
+          </View>
+        </View>
+      </View>
+      );
+  }
+
+}
+
+export default FormMapa;
--- routes/Mapa.tsx
+++ routes/Mapa.tsx
@@ -1,13 +1,124 @@
-import React from "react";
-import { Text, View } from 'react-native';
-import { styles } from "../styles"
+import React, { useState } from "react";
+import { Text, View, Button, Image, StyleSheet, Modal, ListViewBase } from 'react-native';
+import { colors, styles } from "../styles"
+import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
+import { RectButton, TextInput } from "react-native-gesture-handler";
+import {Props} from '../App'
+import FormMapa from '../forms/FormMapa';
+import { string } from "yargs";
 
-function Mapa() {
-    return (
-        <View style={styles.container}>
-            <Text>Aqui jaz um Mapa</Text>
-        </View>
-    );
+const Adicao = require('../assets/images/adicao_button.png')
+const Search = require('../assets/images/searchIcon.png')
+const Pin = require('../assets/images/pinIcon.png')
+
+const mapstyles = StyleSheet.create({
+    rbutton: {
+        width: 60, 
+        height: 60, 
+        resizeMode: 'contain' 
+    },
+    footer: {
+        position: "absolute", 
+        right: 24, 
+        bottom: 53
+    },
+    text_icon: {
+        color: colors.gray,
+        fontWeight: 'bold'
+    },
+    top: {
+        position: "absolute",
+        left: 24,
+        right: 24,
+        top: 32,
+
+        backgroundColor: "#fff",
+        borderRadius: 20,
+        height: 56,
+        paddingLeft: 24,
+
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+
+        elevation: 3,
+    }
+});
+
+class Mapa extends React.Component<Props> {
+
+    state = {
+        region: {
+            latitude: -15.759329042138626,
+            longitude: -47.92363635459222,
+            latitudeDelta: 0.0520,
+            longitudeDelta: 0.0421,
+        },
+        list: [],
+        marker: null,
+        title: undefined,
+        text: undefined
+    }
+
+    render(){
+        const {title, text, list} = this.state;
+        return (
+            <View style={styles.container}>
+                <MapView
+                    provider={PROVIDER_GOOGLE}
+                    style={styles.map}
+                    initialRegion={this.state.region}
+                    onPress={(e) => this.setState({ marker: e.nativeEvent.coordinate })}>
+                    {
+                          this.state.marker &&
+                          <Marker image={Pin} coordinate={this.state.marker} />
+                    }
+                    {
+                        list.map((item, index) => {
+                            return(
+                                <Marker key={index} image={Pin} coordinate={item[2]}>
+                                    <View style={{width: 60, height:60, left:20, bottom: 5}}>
+                                        <Text style={mapstyles.text_icon}>{item[0].substring(0,5)}</Text>
+                                    </View>
+                                </Marker>
+                            );
+                        })
+                    }
+
+                </MapView>
+                <View style={mapstyles.top}>
+                    <TextInput
+                        placeholder={`Pesquise uma localidade`}
+                    />
+                    <RectButton>
+                        <Image style={{width: 56, height: 56}} source={Search} />
+                    </RectButton>
+                </View>
+                <Modal transparent={true} visible={title != undefined && text != undefined}>
+                  <View style={{ backgroundColor: "#000000aa", flex: 1 }}>
+                    <View style={styles.basicFormView}>
+                      <FormMapa
+                        onConfirm={(text, title) => {
+                            if(this.state.marker != null){
+                                this.state.list.push([title, text, this.state.marker])
+                            }
+                            this.setState({title:undefined, text: undefined})
+                        }}
+                        onCancelar={() => this.setState({title: undefined, text: undefined})}
+                        navigation={this.props.navigation}
+                      />
+                    </View>
+                  </View>
+                </Modal>
+                <View style={mapstyles.footer}>
+                    <RectButton onPress={() => this.setState({ text: "", title: "" })}>
+                        <Image style={mapstyles.rbutton} source={Adicao} />
+                    </RectButton>
+                </View>
+                
+            </View>
+        );
+    }
 }
 
 export default Mapa;
